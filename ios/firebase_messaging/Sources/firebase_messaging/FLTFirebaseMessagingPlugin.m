9b6110232a5cc617817814e7bd766b09ee3ed8fc
zhangxueAragon <zhangxue@aragoncs.com>
2026-03-13 11:22:26 +0800

    Update the code to adapt to the Flutter upgrade; the upgraded Flutter version is 3.41.2

--- ios/firebase_messaging/Sources/firebase_messaging/FLTFirebaseMessagingPlugin.m
+++ ios/firebase_messaging/Sources/firebase_messaging/FLTFirebaseMessagingPlugin.m
@@ -88,7 +88,7 @@ NSString *const kMessagingPresentationOptionsUserDefaults =
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
   FlutterMethodChannel *channel =
       [FlutterMethodChannel methodChannelWithName:kFLTFirebaseMessagingChannelName
-                                  binaryMessenger:[registrar messenger]];
+                                  binaryMessenger:[registrar binaryMessenger]];
   id instance = [[FLTFirebaseMessagingPlugin alloc] initWithFlutterMethodChannel:channel
                                                        andFlutterPluginRegistrar:registrar];
   // Register with internal FlutterFire plugin registry.
